43e72c03452693ce561c4bc9e7a7d2de9680f717
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2019-01-09 19:58:25 +0100

    Use RStudio for package development

---
+++ roxygen2.R
@@ -0,0 +1,36 @@
+##
+## (1) Make R packages available
+##
+library(devtools)
+library(roxygen2)
+
+
+##
+## (2) Create documentation file(s) in subdirectory testroxygen/man
+##
+document("../meta")
+
+
+##
+## (3) Build R package and PDF file with help pages
+##
+build("../meta")
+build_manual("../meta")
+
+
+##
+## (4) Install R package
+##
+install("../meta")
+
+
+##
+## (5) Check R package
+##
+check("../meta")
+
+
+##
+## (6) Check R package (with dontrun examples)
+##
+check("../meta", run_dont_test = TRUE)
