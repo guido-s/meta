7ba11b7df23ca6d009023e134bf342160b0d2d8e
Guido Schwarzer <sc@imbi.uni-freiburg.de>
2022-12-13 19:44:41 +0100

    Add missing auxiliary functions for GLMMs and MLMs (#49)

--- R/meta-aux.R
+++ R/meta-aux.R
@@ -331,3 +331,288 @@ setNAwithin <- function(x, condition) {
   ##
   res
 }
+
+
+extrVar <- function(x, name)
+  x[[name]]
+
+
+calcPI <- function(x) {
+  
+  res <- x
+  ##
+  k <- x$k
+  TE.random <- x$TE.random[1]
+  seTE.random <- x$seTE.random[1]
+  method.predict <- x$method.predict
+  level.predict <- x$level.predict
+  ##
+  res$df.predict <- res$upper.predict <- res$lower.predict <-
+    res$seTE.predict <- NA
+  ##
+  tau2.calc <- if (is.na(sum(res$tau2))) 0 else sum(res$tau2)
+  seTE.predict <- sqrt(seTE.random^2 + tau2.calc)
+  ##
+  df.predict <-
+    ifelse(method.predict == "HTS" & k >= 3, k - 2,
+    ifelse(method.predict == "S", Inf, NA))
+  ##
+  ci.p <- ci(TE.random, seTE.predict, level.predict, df = df.predict)
+  ##
+  res$seTE.predict <- rep_len(seTE.predict, length(df.predict))
+  res$lower.predict <- ci.p$lower
+  res$upper.predict <- ci.p$upper
+  res$df.predict <- ci.p$df
+  ##
+  if (length(method.predict) > 1)
+    names(res$seTE.predict) <-
+      names(res$lower.predict) <- names(res$upper.predict) <-
+      names(res$df.predict) <- method.predict
+  ##
+  res
+}
+
+
+runMLM <- function(x, method.tau, method.random.ci, level, ...) {
+  
+  n.methci <- length(method.random.ci)
+  ##
+  list.b <- list(level = 100 * level, method = method.tau,
+                 random = as.call(~ 1 | cluster / idx), ...)
+  ##
+  res <- list.r <- vector("list", n.methci)
+  ##
+  for (i in seq_len(n.methci))
+    list.r[[i]] <- c(x, list.b,
+                     test = ifelse(method.random.ci[i] == "HK", "t", "z"))
+  ##
+  for (i in seq_len(n.methci))
+    res[[i]] <- runNN(rma.mv, list.r[[i]])
+  ##
+  res
+}
+
+extrMLM <- function(x, k, len, sel,
+                    method.random.ci, method.predict,
+                    level.ma, level.predict, null.effect) {
+  
+  ##
+  ## Random effects model(s)
+  ##
+  res <- list(tau2 = NA, tau = NA, se.tau2 = NA)
+  ##
+  res$k <- k
+  res$method.predict <- method.predict
+  res$level.predict <- level.predict
+  ##
+  if (k > 1) {
+    res$tau2 <- x[[1]]$sigma2
+    res$tau <- sqrt(res$tau2)
+  }
+  ##
+  res$TE.random   <- as.numeric(x[[1]]$b)
+  res$seTE.random <- sapply(x, extrVar, "se")
+  ##
+  res$w.random <- rep_len(NA, len)
+  res$w.random[sel] <- weights(x[[1]], type = "rowsum")
+  res$w.random[is.na(res$w.random)] <- 0
+  ##
+  res$lower.random <- sapply(x, extrVar, "ci.lb")
+  res$upper.random <- sapply(x, extrVar, "ci.ub")
+  res$statistic.random <- sapply(x, extrVar, "zval")
+  res$pval.random <-
+    ci(res$TE.random, res$seTE.random,
+       level = level.ma,
+       null.effect = null.effect,
+       df = ifelse(method.random.ci == "HK", k - 1, Inf))$p
+  res$zval.random <- res$statistic.random
+  ##
+  if (length(method.random.ci) > 1)
+    names(res$seTE.random) <-
+      names(res$lower.random) <- names(res$upper.random) <-
+      names(res$statistic.random) <- names(res$pval.random) <-
+      names(res$zval.random) <- method.random.ci
+  ##
+  ## Prediction interval
+  ##
+  res <- calcPI(res)
+  ##
+  res$k <- res$method.predict <- res$level.predict <- NULL
+  ##
+  res
+}
+
+
+runGLMM <- function(x, method.tau, method.random.ci, level,
+                    use.random = TRUE, ...) {
+  
+  n.methci <- length(method.random.ci)
+  ##
+  list.b <- list(level = 100 * level, ...)
+  list.c <- c(x, list.b, method = "FE", test = "z")
+  ##
+  res.r <- list.r <- vector("list", n.methci)
+  ##
+  for (i in seq_len(n.methci))
+    list.r[[i]] <- c(x, list.b, method = method.tau,
+                     test = ifelse(method.random.ci[i] == "HK", "t", "z"))
+  ##
+  res.c <- runNN(rma.glmm, list.c)
+  ##
+  for (i in seq_len(n.methci)) {
+    if (use.random) {
+      res.r[[i]] <-
+        try(runNN(rma.glmm, list.r[[i]]), silent = TRUE)
+      if ("try-error" %in% class(res.r[[i]]))
+        if (grepl(paste0("Number of parameters to be estimated is ",
+                         "larger than the number of observations"),
+                  res.r[[i]]))
+          res.r[[i]] <- res.c
+        else
+          stop(res.r[[i]])
+    }
+    else {
+      ## Fallback to common effect model due to small number of
+      ## studies or zero or all events
+      res.r[[i]] <- res.c
+    }
+  }
+  
+  res <- list(glmm.common = res.c, glmm.random = res.r)
+  ##
+  res
+}
+
+
+addGLMM <- function(x, glmm) {
+  
+  res <- x
+  ##
+  k <- x$k
+  len <- length(x$TE)
+  level.ma <- x$level.ma
+  level.predict <- x$level.predict
+  method.random.ci <- x$method.random.ci
+  method.predict <- x$method.predict
+  null.effect <- replaceNULL(x$null.effect, 0)
+  ##
+  ## Common effect model
+  ##
+  glmm.common <- glmm$glmm.common
+  ##
+  res$TE.common   <- as.numeric(glmm.common$b)
+  res$seTE.common <- as.numeric(glmm.common$se)
+  res$w.common <- rep(NA, len)
+  res$lower.common <- glmm.common$ci.lb
+  res$upper.common <- glmm.common$ci.ub
+  res$statistic.common <- glmm.common$zval
+  res$pval.common <-
+    ci(res$TE.common, res$seTE.common, level = level.ma,
+       null.effect = null.effect)$p
+  res$zval.common <-  res$statistic.common
+  ##
+  ## Random effects model(s)
+  ##
+  glmm.random <- glmm$glmm.random
+  ##
+  res$TE.random   <- as.numeric(glmm.random[[1]]$b)
+  res$seTE.random <- sapply(glmm.random, extrVar, "se")
+  res$w.random <- rep(NA, len)
+  res$lower.random <- sapply(glmm.random, extrVar, "ci.lb")
+  res$upper.random <- sapply(glmm.random, extrVar, "ci.ub")
+  res$statistic.random <- sapply(glmm.random, extrVar, "zval")
+  res$pval.random <-
+    ci(res$TE.random, res$seTE.random,
+       level = level.ma,
+       null.effect = null.effect,
+       df = ifelse(method.random.ci == "HK", k - 1, Inf))$p
+  res$zval.random <- res$statistic.random
+  ##
+  if (length(method.random.ci) > 1)
+    names(res$seTE.random) <-
+      names(res$lower.random) <- names(res$upper.random) <-
+      names(res$statistic.random) <- names(res$pval.random) <-
+      names(res$zval.random) <- method.random.ci
+  ##
+  res$se.tau2 <- res$tau <- res$tau2 <- NA
+  ##
+  if (k > 1) {
+    res$tau2 <- glmm.random[[1]]$tau2
+    res$tau <- sqrt(res$tau2)
+    res$se.tau2 <- glmm.random[[1]]$se.tau2
+  }
+  ##
+  ## Prediction interval
+  ##
+  res <- calcPI(res)
+  ##
+  ## Heterogeneity measures
+  ##
+  gr1 <- glmm.random[[1]]
+  res$Q <- if (gr1$k > 1) gr1$QE.Wld else 0
+  res$df.Q <- gr1$QE.df
+  res$pval.Q <- pvalQ(res$Q, res$df.Q)
+  ##
+  res$Q.LRT <- if (gr1$k > 1) gr1$QE.LRT else 0
+  res$df.Q.LRT <- res$df.Q
+  res$pval.Q.LRT <- pvalQ(res$Q.LRT, res$df.Q.LRT)
+  ##
+  res$upper.tau <- res$lower.tau <- res$upper.tau2 <- res$lower.tau2 <- NA
+  ##
+  res$sign.upper.tau <- res$sign.lower.tau <- res$method.tau.ci <- ""
+  ##
+  H <- calcH(res$Q, res$df.Q, level.ma)
+  res$H <- H$TE
+  res$lower.H <- H$lower
+  res$upper.H <- H$upper
+  ##
+  I2 <- isquared(res$Q, res$df.Q, level.ma)
+  res$I2 <- I2$TE
+  res$lower.I2 <- I2$lower
+  res$upper.I2 <- I2$upper
+  ##
+  res$upper.Rb <- res$lower.Rb <- res$Rb <- NA
+  ##
+  res$.glmm.common <- glmm.common
+  ##
+  if (length(glmm.random) == 1)
+    res$.glmm.random <- glmm.random[[1]]
+  else
+    res$.glmm.random <- glmm.random
+  ##
+  res$version.metafor <- packageDescription("metafor")$Version
+  ##
+  res
+}
+
+
+hccGLMM <- function(x, glmm) {
+  Q.r <- glmm$QE.Wld
+  df.Q.r <- glmm$k - glmm$p
+  ##
+  H.r  <- calcH(Q.r, df.Q.r, x$level.ma)
+  I2.r <- isquared(Q.r, df.Q.r, x$level.ma)
+  ##
+  list(tau2.resid = glmm$tau2,
+       lower.tau2.resid = NA,
+       upper.tau2.resid = NA,
+       ##
+       tau.resid = sqrt(glmm$tau2),
+       lower.tau.resid = NA,
+       upper.tau.resid = NA,
+       sign.lower.tau.resid = "",
+       sign.upper.tau.resid = "",
+       ##
+       Q.resid = Q.r,
+       df.Q.resid = df.Q.r,
+       pval.Q.resid = pvalQ(Q.r, df.Q.r),
+       ##
+       H.resid = H.r$TE,
+       lower.H.resid = H.r$lower,
+       upper.H.resid = H.r$upper,
+       ##
+       I2.resid = I2.r$TE,
+       lower.I2.resid = I2.r$lower,
+       upper.I2.resid = I2.r$upper
+       )
+}
