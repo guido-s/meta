d62698104a5c676dc839a5e907139414d908b7f6
Guido Schwarzer <guido.schwarzer@uniklinik-freiburg.de>
2026-08-10 17:16:53 +0200

    forest.meta(): fix label position if meta-analysis results are not shown

--- R/forest.R
+++ R/forest.R
@@ -11811,7 +11811,7 @@ forest.meta <- function(x,
   #
   # Position on y-axis of left and right labels (at bottom of forest plot)
   #
-  y.bottom.lr <- ymin.line - 2.5 + (!(overall | overall.hetstat) & addrow)
+  y.bottom.lr <- ymin.line - 2.5
   #
   # Position on y-axis of label below x-axis
   #
